3b83a1ba1208ded3cb5e7283761c5a765c7ea9d7
KaKeeM <bella.juju08@gmail.com>
2024-11-06 13:30:30 +0000

    fazendo a busca de o URL da imagem é valido pretendo mudar no futuro pois precisa guardar img no firebase

--- src/components/Missoes/index.tsx
+++ src/components/Missoes/index.tsx
@@ -5,6 +5,14 @@ import { DocumentoItemContainer, Avatar, Content, Timestamp } from './styled';
 interface DocumentoItemProps {
   doc: Documento;
 }
+const isValidUrl = (string: string) => {
+  try {
+    new URL(string);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
 
 const DocumentoItem: React.FC<DocumentoItemProps> = ({ doc }) => {
     const timestamp = doc.timestamp ? new Date(doc.timestamp.seconds * 1000) : null;
@@ -13,7 +21,11 @@ const DocumentoItem: React.FC<DocumentoItemProps> = ({ doc }) => {
     return (
       <DocumentoItemContainer className="documento-item">
         <Avatar className='Avatar'> 
+        {isValidUrl(doc.avatar) ? (
           <img src={doc.avatar} alt={`${doc.name}'s avatar`} />
+        ) : (
+          <img src='https://www.shutterstock.com/image-vector/error-500-page-empty-symbol-260nw-1711106146.jpg'/>
+        )}
         </Avatar>
         <Content className='Content'>
           <h3>{doc.name}</h3>
