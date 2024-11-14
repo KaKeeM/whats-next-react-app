535ea8f7d7233abc0b5a77eacf89daa36b04256d
KaKeeM <bella.juju08@gmail.com>
2024-11-14 18:43:21 +0000

    inserir missão funcionando com  firebase

--- src/components/ListaMissoes/index.tsx
+++ src/components/ListaMissoes/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { addDoc, collection, onSnapshot } from 'firebase/firestore';
 import { auth, db } from '../../firebase';
 import { Documento } from '../Missoes/types';
 import DocumentoItem from '../Missoes/index';
@@ -10,9 +10,7 @@ import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-
 
 const ListaMissoes: React.FC = () => {
   const [documentos, setDocumentos] = useState<Documento[]>([]);
@@ -42,6 +40,36 @@ const ListaMissoes: React.FC = () => {
         return () => unsubscribe();
       }
     }, []);
+  
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const formData = new FormData(event.currentTarget);
+      const formJson = Object.fromEntries(formData.entries());
+
+      if (auth.currentUser) {
+          const userId = auth.currentUser.uid;
+          const userCollectionRef = collection(db, `users/${userId}/MyMissions`);
+          const now = new Date();
+          const formattedTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+          try {
+              await addDoc(userCollectionRef, {
+                  name: formJson.name,
+                  message: formJson.description,
+                  avatar: formJson.avatar,
+                  created_at: now,
+                  timestamp: now,
+                  time: formattedTime
+              });
+              console.log("Missão adicionada com sucesso!");
+          } catch (e) {
+              console.error("Erro ao adicionar missão: ", e);
+          }
+      } else {
+          console.error("Usuário não autenticado");
+      }
+      handleClose();
+  };
 
   return (
     <div>
@@ -55,51 +83,55 @@ const ListaMissoes: React.FC = () => {
       </Container>
 
       <React.Fragment>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: 'form',
-          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries((formData as any).entries());
-            const email = formJson.email;
-            console.log(email);
-            handleClose();
-          },
-        }}
-      >
-        <DialogTitle>Adicionar Missão</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="name"
-            label="Nome da missão"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="description"
-            name="description"
-            label="Descrição"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancelar</Button>
-          <Button type="submit">Criar missão</Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            component: 'form',
+            onSubmit: handleSubmit,
+          }}
+        >
+          <DialogTitle>Adicionar Missão</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="name"
+              name="name"
+              label="Nome da missão"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="description"
+              name="description"
+              label="Descrição"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="avatar"
+              name="avatar"
+              label="Avatar"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancelar</Button>
+            <Button type="submit">Criar missão</Button>
+          </DialogActions>
+        </Dialog>
+      </React.Fragment>
+
       <div className="MenuItemLink">
         {documentos.map((doc) => (
           <DocumentoItem key={doc.id} doc={doc} />
